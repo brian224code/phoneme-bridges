234b182256f0cf49dfee6de995cf244d46821bc0
Brian Le <brianle@mit.edu>
2024-11-01 15:47:21 -0400

    added method to generate romanization dict from wiktionary (no longer needed)

--- clean_urdu_dictionary_script.py
+++ clean_urdu_dictionary_script.py
@@ -1,9 +1,10 @@
 import json
-
-cleaned_dict = {}
+import re
 
 def generate_dict():
-    with open("kaikki.org-dictionary-Urdu.jsonl", encoding="utf-8") as f:
+    cleaned_dict = {}
+
+    with open("../kaikki.org-dictionary-Urdu.jsonl", encoding="utf-8") as f:
         for i, line in enumerate(f):
             data = json.loads(line)
             if "sounds" in data:
@@ -39,6 +40,67 @@ def remove_slashes():
     print("\nExample entries from the updated dictionary:")
     for word, pronunciation in list(updated_dict.items())[:5]:  # Print first 5 entries
         print(f"{word}: {pronunciation}")
+
+
+def generate_romanized_dict():
+    romanization_dict = {}
+
+    with open("../kaikki.org-dictionary-Urdu.jsonl", encoding="utf-8") as f:
+        for i, line in enumerate(f):
+            data = json.loads(line)
+            
+            if "forms" in data:
+                for form in data['forms']:
+                    try:
+                        if 'tags' not in form:
+                            romanization_dict[form['form']] = form['roman']
+                    except:
+                        continue
+
+                    try:
+                        if 'romanization' in form['tags']:
+                            romanization_dict[data['word']] = form['form']
+                        elif 'Hindi' not in form['tags']:
+                            romanization_dict[form['form']] = form['roman']
+                    except:
+                        continue
+
+    cleaned_romanization_dict = {}
+    for word, romanization in romanization_dict.items():
+        if 'span' in word:
+            continue
+        split_words = re.split('، |،| ', word)
+        split_romanizations = re.split(', |,| |-', romanization)
+
+        if len(split_romanizations) == len(split_words):
+            for split_word, split_romanization in zip(split_words, split_romanizations):
+                cleaned_romanization_dict[split_word] = split_romanization
+
+        cleaned_romanization_dict[word] = romanization
+
+    print('len dict: ', len(cleaned_romanization_dict))
+
+
+    # Check hits on missing words with romanization dict
+
+    # count = 0                    
+    # with open("../missing_urdu_words.txt", encoding="utf-8") as f:
+    #     for line in f:
+    #         line = line.rstrip()
+    #         split_line =  re.split('، |،| ', line)
+
+    #         for word in split_line:
+    #             if word not in cleaned_romanization_dict:
+    #                 if line in cleaned_romanization_dict:
+    #                     print(line)
+    #                 break
+    #         else:
+    #             count += 1
+
+    # print('Number of hits: ', count)
+
+            
     
 if __name__ == '__main__':
-    remove_slashes()
+    # remove_slashes()
+    generate_romanized_dict()
